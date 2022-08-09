ae5a5a8fe8a5e38acff848c27470854d95bb7aba
Joseph2792 <joseph.92arce@gmail.com>
2022-08-09 01:39:01 -0300

    animacion de planta y  estilos en la infografia

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
 
-$(window).on("load resize", function() {
-    $('#navbarNav').removeClass("show")
-
-    if (this.matchMedia("(min-width: 768px)").matches) {
-    } else {
-    //$dropdown.off("mouseenter mouseleave");
-    }
-});
-
 $(document).ready(function() {
     $('#btn-toggle-menu').on("click", function() {
         $('#cont-menu').toggle();
         $('.navbar').toggleClass("mobile-menu");
     });
+
+    $(window).on("load resize", function() {
+        $('.navbar').removeClass("mobile-menu");    
+    });
+
+    //var position = $(window).scrollTop(); 
+
+    $(window).scroll(function (){
+        var scroll = $(window).scrollTop();
+        //var alturaDelViewport = window.innerHeight;
+
+        if(scroll > 700){
+            console.log("cambiar logo");
+            $('#logo').attr("src","/assets/logo-color.svg")
+        }else {
+            $('#logo').attr("src","/assets/logo-blanco.svg")
+        }
+
+    });
+
 });
