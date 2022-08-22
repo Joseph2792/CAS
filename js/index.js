6e672d2a43562ffae0998b842d00439bc72c9606
Joseph2792 <joseph.92arce@gmail.com>
2022-08-22 14:21:45 -0300

    fix seccion contacto

--- js/index.js
+++ js/index.js
@@ -9,19 +9,4 @@ $(document).ready(function() {
         $('.navbar').removeClass("mobile-menu");    
     });
 
-    //var position = $(window).scrollTop(); 
-
-    $(window).scroll(function (){
-        var scroll = $(window).scrollTop();
-        //var alturaDelViewport = window.innerHeight;
-
-        if(scroll > 700){
-            console.log("cambiar logo");
-            $('#logo').attr("src","/assets/logo-color.svg")
-        }else {
-            $('#logo').attr("src","/assets/logo-blanco.svg")
-        }
-
-    });
-
 });
