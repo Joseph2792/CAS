f6d84ca375c69df83efdee6084bcb8263b2d22a5
Joseph2792 <joseph.92arce@gmail.com>
2022-08-26 14:00:28 -0300

    correcion de la estructura y menu responsive

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
-$(document).ready(function() {
-    $('#btn-toggle-menu').on("click", function() {
-        $('#cont-menu').toggle();
-        $('.navbar').toggleClass("mobile-menu");
-    });
+// $(document).ready(function() {
+//     $('#btn-toggle-menu').on("click", function() {
+//         $('#cont-menu').toggle();
+//         $('.navbar').toggleClass("mobile-menu");
+//     });
 
-    $(window).on("load resize", function() {
-        $('.navbar').removeClass("mobile-menu");    
-    });
+//     $(window).on("load resize", function() {
+//         $('.navbar').removeClass("mobile-menu");    
+//     });
 
-}); 
+// }); 
+
+// const getElement =  (selector, type = null) => {
+//     if(type == 'all'){
+//         return document.querySelectorAll(selector);
+//     }
+//     return document.querySelector(selector);
+// }
+
+// const mainTitle = getElement(".container-main");
+// console.log(mainTitle);
+
+// const home = document.getElementById('section-home');
+// const sustentabilidad = document.getElementById('section-sustentabilidad');
