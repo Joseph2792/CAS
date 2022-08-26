d1bc928fb7876dc668e52386a7d3c0a4bcd945c4
Joseph2792 <joseph.92arce@gmail.com>
2022-08-26 17:24:00 -0300

    animacion regando

--- js/index.js
+++ js/index.js
@@ -10,15 +10,18 @@
 
 // }); 
 
-// const getElement =  (selector, type = null) => {
-//     if(type == 'all'){
-//         return document.querySelectorAll(selector);
-//     }
-//     return document.querySelector(selector);
-// }
+const getElement =  (selector, type = null) => {
+    if(type == 'all'){
+        return document.querySelectorAll(selector);
+    }
+    return document.querySelector(selector);
+}
 
-// const mainTitle = getElement(".container-main");
-// console.log(mainTitle);
+const mainTitle = getElement(".container-main");
+console.log(mainTitle);
 
-// const home = document.getElementById('section-home');
-// const sustentabilidad = document.getElementById('section-sustentabilidad');
+const home = document.getElementById('section-home');
+const sustentabilidad = document.getElementById('section-sustentabilidad');
+
+
+const opcion_menu = getElement()
