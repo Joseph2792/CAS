fd78e416228326885da2f4c5717777824fd158e6
Joseph2792 <joseph.92arce@gmail.com>
2022-08-28 04:47:49 -0300

    funcionalidad y animacion en el menu desplegable con js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
-// $(document).ready(function() {
-//     $('#btn-toggle-menu').on("click", function() {
-//         $('#cont-menu').toggle();
-//         $('.navbar').toggleClass("mobile-menu");
-//     });
 
-//     $(window).on("load resize", function() {
-//         $('.navbar').removeClass("mobile-menu");    
-//     });
-
-// }); 
-
+//funcion generica para llamar a los elementos del dom
 const getElement =  (selector, type = null) => {
     if(type == 'all'){
         return document.querySelectorAll(selector);
@@ -17,11 +7,49 @@ const getElement =  (selector, type = null) => {
     return document.querySelector(selector);
 }
 
-const mainTitle = getElement(".container-main");
-console.log(mainTitle);
+//llamo a la barras del toggle
+const barsToggle = getElement('.fa-window-minimize','all');
 
-const home = document.getElementById('section-home');
-const sustentabilidad = document.getElementById('section-sustentabilidad');
+//tansformo el boton toggle en close
+const transformarCerrar = () => {
+    for (let i = 0; i < barsToggle.length; i++) {
+        if(i == 0) {
+            barsToggle[i].style.transform = "rotateZ(45deg) translate(20px, 0px)";
+        }
+        if(i == 1){
+            barsToggle[i].style.transform = "rotateZ(-45deg) translate(0px, -22px)";
+        }
+    }
+}
+
+//tansformo el boton close en toggle
+const transformarToggle = () => {
+    for (let i = 0; i < barsToggle.length; i++) {
+        if(i == 0) {
+            barsToggle[i].style.transform = "translate3d(0px, 0px, 10px)";
+        }
+        if(i == 1){
+            barsToggle[i].style.transform = "translate3d(0px, -10px, 10px)";
+        }
+    }
+}
+
+//muestro y oculto el menu
+const menuFlotante = (evt) => {
+    const navbar = getElement('#cont-menu');
+    const tieneClase = navbar.classList.contains('menu-flotante');
+    
+    if(tieneClase){
+        navbar.classList.remove('menu-flotante');
+        transformarToggle();        
+    }else{
+        navbar.classList.add("menu-flotante");
+        transformarCerrar();
+    }        
+}
+
+//obtengo el boton del menu
+const buttonToggle = getElement('#btn-toggle-menu');
+buttonToggle.addEventListener("click", menuFlotante);
 
 
-const opcion_menu = getElement()
